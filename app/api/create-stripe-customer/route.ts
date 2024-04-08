7b34861aa24c72a13c986e325312a68209e553eb
senna-lang <asrsena3302@gmail.com>
2024-04-08 16:06:04 +0900

    add query,supabase.update

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -1,13 +1,28 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 import initStripe from 'stripe';
 
 export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const query = req.nextUrl.searchParams.get('API_ROUTE_SECRET');
+  if (query !== process.env.API_ROUTE_SECRET) {
+    return NextResponse.json({ message: 'Invalid API_SECRET' });
+  }
   const data = await req.json();
-  const { email } = data;
+  const { email, id } = data;
   const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
   const customer = await stripe.customers.create({
     email,
   });
+
+  await supabase
+    .from('profile')
+    .update({
+      stripe_customer: customer.id,
+    })
+    .eq('id', id);
+
   return NextResponse.json({
     message: `stripe customer created ${customer.id}`,
   });
