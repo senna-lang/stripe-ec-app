3c0a52d9fe008f05fefffb426f790db10ada4b9f
senna-lang <asrsena3302@gmail.com>
2024-04-12 20:53:00 +0900

    videoId取得関数

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function extractYouTubeVideoId(url: string): string | null {
+  const matched =
+    /^https?:\/\/(www\.)?youtube\.com\/watch\?(.*&)?v=(?<videoId>[^&]+)/.exec(
+      url
+    ) ??
+    /^https?:\/\/youtu\.be\/(?<videoId>[^?]+)/.exec(url) ??
+    /^https?:\/\/(www\.)?youtube\.com\/embed\/(?<videoId>[^?]+)/.exec(url);
+
+  if (matched?.groups?.videoId) {
+    return matched.groups.videoId;
+  } else {
+    return null;
+  }
+}
