a437f21144276061548ff0a22bcdaabb085a0f36
senna-lang <asrsena3302@gmail.com>
2024-04-16 13:34:37 +0900

    add stripe checkout page

--- components/elements/SubscriptionButton.tsx
+++ components/elements/SubscriptionButton.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { Button } from '@mui/material';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 
 const SubscriptionButton = ({ planId }: { planId: string }) => {
-  const handleSubscribe = async() => {
-    await fetch(`http://localhost:3000/api/subscription/${planId}`);
+  const handleSubscribe = async () => {
+    const res = await fetch(`http://localhost:3000/api/subscription/${planId}`);
+    const data = await res.json();
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
+    await stripe?.redirectToCheckout({ sessionId: data.id });
   };
+
   return <Button onClick={handleSubscribe}>サブスクリプション</Button>;
 };
 
